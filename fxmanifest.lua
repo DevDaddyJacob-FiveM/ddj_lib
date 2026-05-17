24a3955ea84cc95e7698ebd6a73e921389c88f33
DevDaddyJacob <jacobwpaulin@gmail.com>
2026-05-17 15:36:36 -0400

    version: bumped to 1.1.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ use_experimental_fxv2_oal "yes"
 
 author "DevDaddyJacob"
 description "DevDaddyJacob's common FiveM libraries"
-version "1.0.0"
+version "1.1.0"
 
 shared_scripts {
 	"shared/logging.lua",
